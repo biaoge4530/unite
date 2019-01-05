0e191e75599c647bb939c3efdcd02edecf41cd8f
biaoge4530 <957600067@qq.com>
2019-01-05 14:46:10 +0800

    修改首页-biao

---
+++ myapp/src/components/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        home
+        <h2>二组项目</h2>
+        <router-view></router-view>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="">
+    
+</style>
