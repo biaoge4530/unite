c78b8dff86abc56a45628b5c32c2a36bd5d79666
Madanning1 <2931093265@qq.com>
2019-01-08 14:15:01 +0800

    头像第二次提交

--- myapp/src/components/word-mdn/components-head/hot.vue
+++ myapp/src/components/word-mdn/components-head/hot.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="hot">
-        <h2>hot</h2>
+        <div class="hot-left">
+            <div class="hot-left-head"></div>
+        </div>
+        <div class="hot-right"></div>
+       <div
     </div>
 </template>
 <script>
@@ -9,6 +13,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+   .hot{
+     width:90%;
+     min-width:6.75rem;
+     background: pink;
+     margin-left:.4rem; 
+     display: flex;
+     .hot-left{
+         min-height: 1rem;
+         background: #cdcdcd;
+         width:27%; 
+         .hot-left-head{
+             width:1rem; 
+         }
+     }
+     .hot-right{
+         min-height: 1rem;
+         background:red;
+         width:73%; 
+     }
+   }
 </style>
+
--- myapp/src/components/word-mdn/components-head/nav.vue
+++ myapp/src/components/word-mdn/components-head/nav.vue
@@ -1,39 +1,67 @@
 <template>
-    <div class="nav">
-        <!-- 头像的 内容区域 -->
-    <div class="top-nav"> 
-        <div class="top-nav-hot">
-           <router-link :to="{name:'hot'}"> 动态</router-link>
-         </div>
-        <div class="top-nav-notebook">
-            <router-link :to="{name:'notebook'}"> 手账</router-link>
-        </div>   
+  <div class="nav">
+    <!-- 头像的 内容区域 -->
+    <div class="top-nav">
+      <div class="top-nav-hot" @click="handleTo(1)">动态
+        <!-- <router-link :to="{name:'hot'}"> </router-link> -->
+      </div>
+      <div class="top-nav-notebook" @click="handleTo(2)">手账
+        <!-- <router-link :to="{name:'notebook'}"> </router-link> -->
+      </div>
     </div>
-    <router-view></router-view>
-    </div>
+    <keep-alive>
+      <component :is="conName"></component>
+    </keep-alive>
+    <!-- <router-view></router-view> -->
+  </div>
 </template>
 <script>
-
+import hot from "./hot";
+import notebook from "./notebook";
 export default {
-    
-}
+  components:{
+      "Hot-com":hot,
+      "Notebook-com":notebook
+  },
+  data(){
+    return {
+      conName:"Hot-com",
+      activeindex:0
+    };
+  },
+  methods: {
+    handleTo(index) {    
+      if(index == 1){
+         this.conName = "Hot-com";
+      }else if(index == 2){
+         this.conName = "Notebook-com";
+      }
+      
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-  // 详情内容
-.top-nav{
-    width: 100%;
-    height: .76rem;
-    background: #cccccc;
-    display: flex;
-    .top-nav-hot{
-        width: 50%;
-        height: .76rem;
-        background: #9977bb;
-    }
-    .top-nav-notebook{
-        width: 50%;
-        height: .76rem;
-        background:#33ffee
-    }
+// 详情内容
+.top-nav {
+  width: 100%;
+  height: 0.76rem;
+  display: flex;
+  font-size: 0.27rem;
+  background: pink;
+  font-weight: bold;
+  color: #3eb59f;
+  .top-nav-hot {
+    width: 50%;
+    height: 0.76rem;
+    line-height:.76rem; 
+    text-align:center; 
+  }
+  .top-nav-notebook {
+    width: 50%;
+    height: 0.76rem;
+    line-height:.76rem; 
+    text-align:center; 
+  }
 }
 </style>
