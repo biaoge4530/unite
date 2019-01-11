ee2a05be8f14ea38c5aa0026dae03e89d007546b
Madanning1 <2931093265@qq.com>
2019-01-11 14:44:41 +0800

    ‘“冲突

--- myapp/src/components/word-mdn/headphoto.vue
+++ myapp/src/components/word-mdn/headphoto.vue
@@ -11,13 +11,13 @@ import Top from "./components-head/top"
 import Nav from "./components-head/nav"
 import Hot from "./components-head/hot"
 import Notebook from "./components-head/notebook"
-import BScroll from 'better-scroll'
+
 export default {
   components:{
     "Top-com":Top,
     "Nav-com":Nav,
     "Hot-com":Hot,
-    "BScroll-com":BScroll
+ 
   }
 }
 </script>
